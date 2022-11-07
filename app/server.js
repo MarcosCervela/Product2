36919064764cacdd6f5da69cce5dcc9f1da26a84
MarcosCervela <101353178+MarcosCervela@users.noreply.github.com>
2022-11-07 17:21:37 +0100

    conexión con la base de datos

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectionString = 'mongodb+srv://JavaScript:uoc1234@producto2.prmbfgr.mongodb.net/test'
 
-
+//Conexión con la base de datos
 mongoose.connect(connectionString)
 
     .then(() => {
